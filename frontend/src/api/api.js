1d5cc5382eb3385e7fddc2f75587718465b97f65
shai-yona <shaiyonabusiness@example.com>
2025-04-05 20:40:22 +0300

    register success

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
+
 // Create an axios instance
 const api = axios.create({
   baseURL: BASE_URL,
